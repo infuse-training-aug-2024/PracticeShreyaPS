91023cbd1b9529b6bc379f42b1b7244626469466
shreya <89719271+ShreyaPSawant@users.noreply.github.com>
2024-08-22 17:24:18 +0530

    reset tasks applied

--- calculator.py
+++ calculator.py
@@ -3,6 +3,8 @@ class Calculator:
         return num1 + num2
     def sub(self, num1, num2):
         return num1 - num2
+    def mul(self, num1, num2):
+        return num1 * num2
 
 # Creating an instance of the Calculator class
 calc = Calculator()
@@ -10,5 +12,6 @@ calc = Calculator()
 # Using the add method to add two numbers
 result = calc.add(10, 5)
 result = calc.sub(10, 5)
+result = calc.mul(10, 5)
 
 print("The sum is:", result)
