bb7a9f71bd1acc820b03af1b60db9c2e5cdd26e5
shreya <89719271+ShreyaPSawant@users.noreply.github.com>
2024-08-22 17:19:31 +0530

    sub func added

--- calculator.py
+++ calculator.py
@@ -1,11 +1,14 @@
 class Calculator:
     def add(self, num1, num2):
         return num1 + num2
+    def sub(self, num1, num2):
+        return num1 - num2
 
 # Creating an instance of the Calculator class
 calc = Calculator()
 
 # Using the add method to add two numbers
 result = calc.add(10, 5)
+result = calc.sub(10, 5)
 
 print("The sum is:", result)
