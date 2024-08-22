69e2ff8a704f700dd4930ba1796a6d8a5d183034
shreya <89719271+ShreyaPSawant@users.noreply.github.com>
2024-08-22 17:18:20 +0530

    initialized and added add func

---
+++ calculator.py
@@ -0,0 +1,11 @@
+class Calculator:
+    def add(self, num1, num2):
+        return num1 + num2
+
+# Creating an instance of the Calculator class
+calc = Calculator()
+
+# Using the add method to add two numbers
+result = calc.add(10, 5)
+
+print("The sum is:", result)
