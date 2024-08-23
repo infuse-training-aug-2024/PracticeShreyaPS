40c9332aea8c8d742a9af433b2691cded6036f6a
shreya <89719271+ShreyaPSawant@users.noreply.github.com>
2024-08-23 16:17:46 +0530

    exercise 3 complete

---
+++ HTML/ex3/script3.js
@@ -0,0 +1,19 @@
+const user = {
+    name: "Piyush Sharma",
+    designation: "Senior Software Engineer",
+    company: "Infuse Consulting",
+    hobbies: ["Reading", "Listening to music", "Collecting stamps"]
+}
+
+let {name, designation,company,hobbies}=user;
+const len=hobbies.length;
+const firstSetHobbies=hobbies.slice(0,len-1).join(",");
+const LastHobbies=hobbies.slice(2);
+
+const printUserProfile = () => {
+    // Piyush Sharma is a Senior Software Engineer at Infuse Consulting. He likes Reading, Listening to music and Collecting stamps
+    
+    console.log(`${name} is a ${designation} at ${company}. He likes ${firstSetHobbies} and ${LastHobbies}`);
+}
+
+printUserProfile()
