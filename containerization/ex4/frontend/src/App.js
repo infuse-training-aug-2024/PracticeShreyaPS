41d4a385cd031e91c91b82211b86d24931ad5f03
shreya <89719271+ShreyaPSawant@users.noreply.github.com>
2024-09-20 10:15:15 +0530

    ex4 working

--- containerization/ex4/frontend/src/App.js
+++ containerization/ex4/frontend/src/App.js
@@ -17,7 +17,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Open console and refresh to see response from backend 
         </p>
         <a
           className="App-link"
