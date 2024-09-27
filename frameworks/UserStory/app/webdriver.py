7280fbc9184bcfc692ae0de21bf93c0d122d5408
shreya sawant <spsawant31@gmail.com>
2024-09-27 17:47:29 +0530

    working on userstory 3

--- frameworks/UserStory/app/webdriver.py
+++ frameworks/UserStory/app/webdriver.py
@@ -14,29 +14,45 @@
 #         self.driver =webdriver.Chrome(service=service,options=option)
 
 
-# from selenium import webdriver
-# from selenium.webdriver.chrome.service import Service
-# from selenium.webdriver.chrome.options import Options
-# from webdriver_manager.chrome import ChromeDriverManager
-
-# class WebDriver:
-#     def __init__(self):
-#         # Automatically download and use the correct version of chromedriver
-#         service = Service(ChromeDriverManager().install())
-#         options = Options()
-#         options.add_argument('--no-sandbox')
-#         options.add_argument('--disable-dev-shm-usage')
-#         self.driver = webdriver.Chrome(service=service, options=options)
-
-
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
 class WebDriver:
     def __init__(self):
-        options = webdriver.ChromeOptions()
-        options.add_argument("--no-sandbox")   
-        options.add_argument("--disable-dev-shm-usage")  # Overcome limited resource problems
-        options.add_argument("--disable-gpu")  # Disable GPU acceleration (optional)
+        # Automatically download and use the correct version of chromedriver
+        service = Service(ChromeDriverManager().install())
+        options = Options()
+        options.add_argument("--headless")
+        options.add_argument('--no-sandbox')
+        options.add_argument('--disable-dev-shm-usage')
+        self.driver = webdriver.Chrome(service=service, options=options)
 
-        self.driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+
+# from selenium import webdriver
+# from webdriver_manager.chrome import ChromeDriverManager
+
+# class WebDriver:
+#     def __init__(self):
+#         options = webdriver.ChromeOptions()
+#         options.add_argument("--no-sandbox")   
+#         options.add_argument("--disable-dev-shm-usage")  # Overcome limited resource problems
+#         options.add_argument("--disable-gpu")  # Disable GPU acceleration (optional)
+
+#         self.driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+# from selenium import webdriver
+# from selenium.webdriver.chrome.service import Service
+# from webdriver_manager.chrome import ChromeDriverManager
+# from selenium.webdriver.chrome.options import Options
+
+# class WebDriver:
+#     def __init__(self):
+#         chrome_options = Options()
+#         chrome_options.add_argument("--headless")  # Ensure headless mode is enabled
+#         chrome_options.add_argument("--disable-gpu")
+#         chrome_options.add_argument("--no-sandbox")
+#         chrome_options.add_argument("--disable-dev-shm-usage")
+
+#         service = Service(ChromeDriverManager().install())
+#         self.driver = webdriver.Chrome(service=service, options=chrome_options)
